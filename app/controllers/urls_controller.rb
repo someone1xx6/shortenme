a8cfdd066f4f0b0b71e3bfc6456487faf4698e35
someone1xx6 <someone1xx6@gmail.com>
2017-02-10 18:22:31 +0700

    add environment variables

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -18,7 +18,7 @@ class UrlsController < ApplicationController
   end
 
   def create
-  	#@url = Url.new(params.require(:shorten).permit(:original_url))
+  	@url = Url.new(params.require(:shorten).permit(:original_url))
   	#@url.save
 
 	   #redirect_to "https://#{ENV["BASE_URL"]}#{@url.short_url}"+'+'
